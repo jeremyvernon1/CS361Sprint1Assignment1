59e32c05472fb5681f08336cb4039ef6bce38dd7
jeremyvernon1 <jeremyvernon@icloud.com>
2021-09-27 06:31:53 -0400

    Initial commit

---
+++ Assignment 1.py
@@ -0,0 +1,9 @@
+# Author: Jeremy Vernon
+# Date: 27 September, 2021
+# Description: Sprint 1 Assignment 1 Test Commit
+
+# Declare variables
+VariableOne = "\nThis is a test. This is only a test.\n"
+
+# Display message
+print(VariableOne)
